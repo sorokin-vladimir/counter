ba065980ecf5beb2945b6af19dd189fa4374e524
Vladimir Sorokin <v.sorokin@hey.com>
2023-11-10 13:22:51 +0200

    Added gh-pages

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ export function App() {
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
